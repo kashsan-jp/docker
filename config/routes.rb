acda814f3830fde220dd2647317e83afd3d00185
Kashsan <kashsan_jp@yahoo.co.jp>
2025-01-05 01:23:55 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :recipes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :posts, only: [:index, :new, :create]
+  resources :posts, only: [:index, :new, :create, :edit, :update, :destroy]
 end
