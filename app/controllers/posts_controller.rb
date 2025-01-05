aa2a1ec7043742f38bdd177acd4fa2f689ecf744
Kashsan <kashsan_jp@yahoo.co.jp>
2025-01-05 10:47:01 +0900

    “変更確認願います”

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:edit, :update, :destroy]
+
     def index
         @posts = Post.all
     end
@@ -18,16 +20,22 @@ class PostsController < ApplicationController
     end
 
     def edit
-        @post = Post.find(params[:id])
+        # @post = Post.find(params[:id])
     end
 
     def update
-        @post = Post.find(params[:id])
+        # @post = Post.find(params[:id])
         if @post.update(post_params)
             redirect_to posts_path
         else
             render :edit 
         end
+    end
+
+    def destroy
+        # @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
     end
 
     private
@@ -36,4 +44,7 @@ class PostsController < ApplicationController
         params.require(:post).permit(:title, :content)
     end
     
+    def set_post
+        @post = Post.find(params[:id])
+    end
 end
